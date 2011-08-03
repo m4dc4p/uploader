a5ecaf8d88d23e0a985cd69a592451909bf38fba
Justin Bailey <jgbailey@gmail.com>
2011-08-03 16:46:24 -0700

    Code cleanup

--- src/file/data/File.js
+++ src/file/data/File.js
@@ -1,27 +1,32 @@
 Ext.require('Ext.data.Model');
 
-Ext.define('Cs.file.data.File', {
-  extend: 'Ext.data.Model',
-  idProperty: 'id',
-  statics: {
-    FORM: 'form',
-    FILE: 'file',
-  },
-  fields: [{
-    name: 'id', type: 'int' 
-  }, {
-    name: 'name', type: 'string' 
-  }, {
-    name: 'size', type: 'int' 
-  }, {
-    name: 'type', type: 'string'
-  }, {
-    name: 'data', type: 'auto'
-  }],
-  validations: [{
-    type: 'inclusion', field: 'type', list: ['form', 'file']
-  }],
-  // Will hold the actual file or form reference
-  raw: null
-});
+Ext.define('Cs.file.data.File', (function() {
+  var FORM = 'form',
+  FILE = 'file';
+  
+  return {
+    extend: 'Ext.data.Model',
+    idProperty: 'id',
+    statics: {
+      FORM: FORM,
+      FILE: FILE,
+    },
+    fields: [{
+      name: 'id', type: 'int' 
+    }, {
+      name: 'name', type: 'string' 
+    }, {
+      name: 'size', type: 'int' 
+    }, {
+      name: 'type', type: 'string'
+    }, {
+      name: 'data', type: 'auto'
+    }],
+    validations: [{
+      type: 'inclusion', field: 'type', list: [FORM, FILE]
+    }],
+    // Will hold the actual file or form reference
+    raw: null
+  };
+})());
 
